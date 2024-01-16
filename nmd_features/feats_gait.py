f3005425c3e9ca4fff776fd3601cd7b5fb220f7d
parkersruth <psr23@uw.edu>
2024-01-15 22:18:40 -0800

    updating gait features

--- nmd_features/feats_gait.py
+++ nmd_features/feats_gait.py
@@ -4,41 +4,50 @@ from pathlib import Path
 
 import numpy as np
 import pandas as pd
+import scipy.signal as ss
 
 from numpy.linalg import norm
 
 from utilsLoaders import read_trc, read_mot
-from utils import center_of_mass
+from utils import center_of_mass, center_of_mass_vel
 
 
-def gait_trc_feats(xyz, markers, fps, com, trial_clean):
+def gait_trc_feats(xyz, markers, fps, com, comv, trial_clean):
     # com = xyz[:,np.argmax(markers == 'midHip'),:] # TODO uses actual CoM
-    com -= com[-1,:]
-
-    com_dist = norm(com[:,[0,2]], axis=-1)
-    # last_3m = np.argmax(com_dist < 4)
-    last_3m = np.argmax((1 < com_dist) & (com_dist < 4))
-    time_3m = (xyz.shape[0] - last_3m)/fps
-    time_10m = time_3m * 10 / 3
-    speed = 3 / time_3m
-
-    com_bob = com[last_3m:,1].ptp()
-
-    com_xz = com[last_3m:,[0,2]].copy()
+    com -= com[-2,:]
+    
+    # compute direction of travel
+    com_xz = com[:,[0,2]].copy()
     direction = com_xz[0,:] - com_xz[-1,:]
     direction /= norm(direction)
-    com_xz -= np.outer(com_xz @ direction, direction)
-    com_sway = norm(com_xz, axis=1).ptp()
-
-    # TODO scale bob and sway by height
-
+    
+    # compute lateral sway
+    comv_xz = comv[:,[0,2]].copy()
+    comv_xz -= np.outer(comv_xz @ direction, direction)
+    com_sway = comv_xz[:,0]
+    win = ss.windows.hann(int(0.5*fps))
+    win /= np.sum(win)
+    com_sway = ss.convolve(com_sway, win, mode='same')
+    
+    # compute usable kinematics zone
+    com_dist = norm(com[:,[0,2]], axis=-1)
+    zone_start = np.argmax(com_dist < 4)
+    zone_stop = np.argmax(com_dist < 1)
+    zone = np.arange(xyz.shape[0])
+    zone = (zone >= zone_start) & (zone < zone_stop)
+    
+    # compute metrics
+    time_3m = (zone_stop-zone_start)/fps
+    time_10m = time_3m * 10 / 3
+    speed = 3 / time_3m
+    com_sway = com_sway[zone].std()
+    
     # TODO joint impedance? See Cavallo 2022
-
+    
     # return time_10m, speed, com_bob, com_sway, last_3m
     return {
             f'{trial_clean}_time_10m': float(time_10m),
             f'{trial_clean}_speed': float(speed),
-            f'{trial_clean}_com_bob': float(com_bob),
             f'{trial_clean}_com_sway': float(com_sway),
            }
 
@@ -78,8 +87,9 @@ def gait_mot_feats(df, trial_clean):
 
 def feats_gait(trc_fpath, mot_fpath, model_fpath, trial_clean='gait'):
     fps, markers, xyz = read_trc(trc_fpath)
-    com_xyz = center_of_mass(model_fpath, mot_fpath)
-    trc_feats = gait_trc_feats(xyz, markers, fps, com_xyz, trial_clean)
+    com = center_of_mass(model_fpath, mot_fpath)
+    comv = center_of_mass_vel(model_fpath, mot_fpath)
+    trc_feats = gait_trc_feats(xyz, markers, fps, com, comv, trial_clean)
 
     df = read_mot(mot_fpath)
     mot_feats = gait_mot_feats(df, trial_clean)
