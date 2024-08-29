b2fa9426b7563f60fcf9a8d73e29679e5adbbc75
parkersruth <psr23@uw.edu>
2024-08-29 15:11:26 -0700

    checkpoint

--- nmd_features/feats_5xsts.py
+++ nmd_features/feats_5xsts.py
@@ -102,9 +102,9 @@ def sts_trc_feats(xyz, markers, fps):
             '5xsts_time_5': float(sts_time_5),
             # '5xsts_num': float(len(locs)),
             # '5xsts_speed': float(sts_speed),
-            '5xsts_lean_ptp': float(lean_ptp),
-            '5xsts_lean_max': float(lean_max),
-            # '5xsts_lean_avel': float(lean_avel),
+            # '5xsts_lean_ptp': float(lean_ptp),
+            # '5xsts_lean_max': float(lean_max),
+            '5xsts_lean_avel': float(lean_avel),
             # '5xsts_mean_hand_elev': float(mean_hand_elev),
             '5xsts_stance_width': float(stance_width),
            }
